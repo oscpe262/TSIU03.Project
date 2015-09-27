103316310cdd20ad0329a09d9d7748dd42f1af41
oscpe262 <oscpe262@student.liu.se>
2015-09-27 16:04:39 +0200

    foo

--- Lab4_Oscar_Matteus/Ctrl.vhd
+++ Lab4_Oscar_Matteus/Ctrl.vhd
@@ -3,32 +3,32 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity Ctrl is
-	port( clk, rstn		: in std_logic;					-- System Clock and Reset on low
-			mclk, bclk 		: out std_logic; 					-- Master Clock and Bit Clock
-			adclrc, daclrc : out std_logic;					-- ADC/DAC Left/Right Channel Control
-			men, lrsel		: out std_logic; 					-- Master Enable and Left/Right Select (for SoundBus)
-			SCCnt 			: out unsigned(1 downto 0);	-- Sub Cycle Counter
-			BitCnt			: out unsigned(4 downto 0));	-- Bit Counter
+  port( clk, rstn		: in std_logic;			-- System Clock and Reset on low
+        mclk, bclk 		: out std_logic; 		-- Master Clock and Bit Clock
+        adclrc, daclrc          : out std_logic;		-- ADC/DAC Left/Right Channel Control
+        men, lrsel		: out std_logic; 		-- Master Enable and Left/Right Select (for SoundBus)
+        SCCnt 			: out unsigned(1 downto 0);	-- Sub Cycle Counter
+        BitCnt			: out unsigned(4 downto 0));	-- Bit Counter
 end entity;
 
 architecture rtl of Ctrl is
-	signal counter		: unsigned(9 downto 0);
+  signal counter		: unsigned(9 downto 0);
 begin
-	process(clk, rstn)
-	begin
-		if rstn = '0' then
-			counter <= (others => '0');
-		elsif rising_edge(clk) then
-			counter <= counter + 1;
-		end if;
-	end process;
-	
-	mclk 		<= NOT counter(1);
-	bclk 		<= counter(3);
-	adclrc	<= NOT counter(9);
-	daclrc	<= NOT counter(9);
-	men 		<= counter(0) AND counter(1);
-	lrsel		<= counter(9);
-	SCCnt 	<= counter(3 downto 2);
-	BitCnt 	<= counter(8 downto 4);
+  process(clk, rstn)
+  begin
+    if rstn = '0' then
+      counter <= (others => '0');
+    elsif rising_edge(clk) then
+      counter <= counter + 1;
+    end if;
+  end process;
+  
+  mclk 		<= NOT counter(1);
+  bclk 		<= counter(3);
+  adclrc	<= NOT counter(9);
+  daclrc	<= NOT counter(9);
+  men 		<= counter(0) AND counter(1);
+  lrsel		<= counter(9);
+  SCCnt 	<= counter(3 downto 2);
+  BitCnt 	<= counter(8 downto 4);
 end rtl;
--- Lab4_Oscar_Matteus/Sound.vhd
+++ Lab4_Oscar_Matteus/Sound.vhd
@@ -23,59 +23,59 @@ USE ieee.numeric_std.all;
 LIBRARY work;
 
 ENTITY Sound IS 
-	PORT
-	(
-		clk :  IN  STD_LOGIC;
-		rstn :  IN  STD_LOGIC;
-		adcdat :  IN  STD_LOGIC;
-		SW :  IN  STD_LOGIC_VECTOR(5 TO 7);
-		mclk :  OUT  STD_LOGIC;
-		bclk :  OUT  STD_LOGIC;
-		adclrc :  OUT  STD_LOGIC;
-		daclrc :  OUT  STD_LOGIC;
-		dacdat :  OUT  STD_LOGIC;
-		HEX6 :  OUT  STD_LOGIC_VECTOR(0 TO 6);
-		HEX7 :  OUT  STD_LOGIC_VECTOR(0 TO 6);
-		LEDR :  OUT  STD_LOGIC_VECTOR(0 TO 17)
-	);
+  PORT
+    (
+      clk :  IN  STD_LOGIC;
+      rstn :  IN  STD_LOGIC;
+      adcdat :  IN  STD_LOGIC;
+      SW :  IN  STD_LOGIC_VECTOR(5 TO 7);
+      mclk :  OUT  STD_LOGIC;
+      bclk :  OUT  STD_LOGIC;
+      adclrc :  OUT  STD_LOGIC;
+      daclrc :  OUT  STD_LOGIC;
+      dacdat :  OUT  STD_LOGIC;
+      HEX6 :  OUT  STD_LOGIC_VECTOR(0 TO 6);
+      HEX7 :  OUT  STD_LOGIC_VECTOR(0 TO 6);
+      LEDR :  OUT  STD_LOGIC_VECTOR(0 TO 17)
+      );
 END Sound;
 
 ARCHITECTURE bdf_type OF Sound IS 
 
   COMPONENT group_no
-  GENERIC (number : INTEGER);
+    GENERIC (number : INTEGER);
     PORT(HEX6 : OUT STD_LOGIC_VECTOR(0 TO 6);
          HEX7 : OUT STD_LOGIC_VECTOR(0 TO 6));
   END COMPONENT;
 
   COMPONENT application
     PORT(clk : IN STD_LOGIC;
-       rstn : IN STD_LOGIC;
-       lrsel : IN STD_LOGIC;
-       LADC : IN SIGNED(15 DOWNTO 0);
-       RADC : IN SIGNED(15 DOWNTO 0);
-       SW : IN STD_LOGIC_VECTOR(5 TO 7);
-       LDAC : OUT SIGNED(15 DOWNTO 0);
-       LEDR : OUT STD_LOGIC_VECTOR(0 TO 17);
-       RDAC : OUT SIGNED(15 DOWNTO 0)
-    );
+         rstn : IN STD_LOGIC;
+         lrsel : IN STD_LOGIC;
+         LADC : IN SIGNED(15 DOWNTO 0);
+         RADC : IN SIGNED(15 DOWNTO 0);
+         SW : IN STD_LOGIC_VECTOR(5 TO 7);
+         LDAC : OUT SIGNED(15 DOWNTO 0);
+         LEDR : OUT STD_LOGIC_VECTOR(0 TO 17);
+         RDAC : OUT SIGNED(15 DOWNTO 0)
+         );
   END COMPONENT;
 
   COMPONENT snddriver
     PORT(clk : IN STD_LOGIC;
-       rstn : IN STD_LOGIC;
-       adcdat : IN STD_LOGIC;
-       LDAC : IN SIGNED(15 DOWNTO 0);
-       RDAC : IN SIGNED(15 DOWNTO 0);
-       mclk : OUT STD_LOGIC;
-       bclk : OUT STD_LOGIC;
-       adclrc : OUT STD_LOGIC;
-       daclrc : OUT STD_LOGIC;
-       lrsel : OUT STD_LOGIC;
-       dacdat : OUT STD_LOGIC;
-       LADC : OUT SIGNED(15 DOWNTO 0);
-       RADC : OUT SIGNED(15 DOWNTO 0)
-    );
+         rstn : IN STD_LOGIC;
+         adcdat : IN STD_LOGIC;
+         LDAC : IN SIGNED(15 DOWNTO 0);
+         RDAC : IN SIGNED(15 DOWNTO 0);
+         mclk : OUT STD_LOGIC;
+         bclk : OUT STD_LOGIC;
+         adclrc : OUT STD_LOGIC;
+         daclrc : OUT STD_LOGIC;
+         lrsel : OUT STD_LOGIC;
+         dacdat : OUT STD_LOGIC;
+         LADC : OUT SIGNED(15 DOWNTO 0);
+         RADC : OUT SIGNED(15 DOWNTO 0)
+         );
   END COMPONENT;
 
   SIGNAL	LADC :  SIGNED(15 DOWNTO 0);
@@ -87,34 +87,34 @@ ARCHITECTURE bdf_type OF Sound IS
 BEGIN 
 
   b2v_inst : group_no
-  GENERIC MAP(number => 99)
-  PORT MAP(HEX6 => HEX6,
-           HEX7 => HEX7);
+    GENERIC MAP(number => 99)
+    PORT MAP(HEX6 => HEX6,
+             HEX7 => HEX7);
   
   b2v_instApp : application
-  PORT MAP(clk => clk,
-       rstn => rstn,
-       lrsel => lrsel,
-       LADC => LADC,
-       RADC => RADC,
-       SW => SW,
-       LDAC => LDAC,
-       LEDR => LEDR,
-       RDAC => RDAC);
+    PORT MAP(clk => clk,
+             rstn => rstn,
+             lrsel => lrsel,
+             LADC => LADC,
+             RADC => RADC,
+             SW => SW,
+             LDAC => LDAC,
+             LEDR => LEDR,
+             RDAC => RDAC);
   
   b2v_instSndDrv : snddriver
-  PORT MAP(clk => clk,
-       rstn => rstn,
-       adcdat => adcdat,
-       LDAC => LDAC,
-       RDAC => RDAC,
-       mclk => mclk,
-       bclk => bclk,
-       adclrc => adclrc,
-       daclrc => daclrc,
-       lrsel => lrsel,
-       dacdat => dacdat,
-       LADC => LADC,
-       RADC => RADC);
+    PORT MAP(clk => clk,
+             rstn => rstn,
+             adcdat => adcdat,
+             LDAC => LDAC,
+             RDAC => RDAC,
+             mclk => mclk,
+             bclk => bclk,
+             adclrc => adclrc,
+             daclrc => daclrc,
+             lrsel => lrsel,
+             dacdat => dacdat,
+             LADC => LADC,
+             RADC => RADC);
   
 END bdf_type;
